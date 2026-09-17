6ee52269c82bbd2e6b426de628d1b69ef9706f8e
Alexander Crosado (1000050232) <CROSAD1@student.op.ac.nz>
2026-09-17 16:28:25 +1200

    This one should work

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace HelloCode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("This is a test");
+            Console.ReadLine();
+
+        }
+    }
+}
